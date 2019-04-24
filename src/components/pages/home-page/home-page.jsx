d30b7d1ea40225d341bc52d412bcf0b47759e809
soia <nikita.soya@gmail.com>
2019-04-24 13:57:00 +0200

    finish home page

--- src/components/pages/home-page/home-page.jsx
+++ src/components/pages/home-page/home-page.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 import style from "./home-page.module.scss";
 
@@ -7,32 +8,45 @@ import appStore from "./images/appStore.svg";
 import androidStore from "./images/androidStore.svg";
 
 const HomePage = () => {
-    return (
-        <div className={style.homepage}>
-            <div className={style.homepage__leftSide}>
-                <div className={style.homepage__hub}>
-                    <h1>MHub</h1>
-                </div>
+  const { t } = useTranslation();
 
-                <p className={style.homepage__description}>Lorem ipsum dolor sit amet, consectetur orem ipsum dolor sit amet, consectetur orem ipsum dolor sit amet,
-                    consectetur orem ipsum dolor sit amet, consectetur orem ipsum dolor sit amet, consectetur 
-                </p>
+  return (
+    <div className={style.homepage}>
+        <div className={style.homepage__leftSide}>
 
-                <div className={style.homepage__socialLinks}>
-                    <a href="https://www.apple.com/itunes/" target="_blank" rel="noopener noreferrer">
-                        <img src={appStore} alt="appStore" />
-                    </a>
-                    <a href="https://play.google.com/store/" target="_blank" rel="noopener noreferrer">
-                        <img src={androidStore} alt="androidStore" />
-                    </a>
-                </div>
-
+            <div
+                className={style.homepage__hub}>
+                <h1>MHub</h1>
             </div>
-            <div className={style.homepage__leftSide}>
-                <img src={mobile} alt="mobile" />
+
+            <p
+                className={style.homepage__description}>
+                {t("homePage.description")}
+            </p>
+
+            <div className={style.homepage__socialLinks}>
+                <a
+                    href="https://www.apple.com/itunes/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <img src={appStore} alt="appStore" />
+                </a>
+                <a
+                    href="https://play.google.com/store/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <img src={androidStore} alt="androidStore" />
+                </a>
             </div>
         </div>
-    );
+
+        <div className={style.homepage__leftSide}>
+            <img src={mobile} alt="mobile" />
+        </div>
+    </div>
+  );
 };
 
 export default HomePage;
